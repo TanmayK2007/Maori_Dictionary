9fd174996455c2486c100187de035e3a62d1756d
22452 <22452@wc.school.nz>
2024-03-27 15:15:24 +1300

    day 1 work, got a lot of work done today, pretty proud of it.

--- app.py
+++ app.py
@@ -24,10 +24,100 @@ def create_connection(db_file):
     return None
 
 
+def is_logged_in():
+    if session.get("email") is None:
+        print("not logged in")
+        return False
+    else:
+        print("logged in")
+        return True
+
+
 @app.route('/')
 def render_home():
-    message = request.args.get('message')
-    if message is None:
-        message = ""
-
     return render_template('home.html')
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def render_login():
+    if is_logged_in():
+        return redirect('/')
+    print("Logging in")
+    if request.method == "POST":
+        email = request.form['email'].strip().lower()
+        password = request.form['password'].strip()
+        print(email)
+        query = """SELECT id, fname, password FROM user WHERE email = ?"""
+        con = create_connection(DATABASE)
+        cur = con.cursor()
+        cur.execute(query, (email,))
+        user_data = cur.fetchall()
+        con.close()
+        print(user_data)
+        # if given email is not in the database this will raise an error
+        # would be better to find out how to see if the query return an empty result set
+        if user_data is None:
+            return redirect("/login?error=Email+invalid+password+incorrect")
+
+        user_id = user_data[0][0]
+        first_name = user_data[0][1]
+        db_password = user_data[0][2]
+
+        # check if the password is incorrect for that email address
+
+        if not bcrypt.check_password_hash(db_password, password):
+            return redirect(request.referrer + "?error=Email+invalid+or+password+incorrect")
+
+        session['email'] = email
+        session['user_id'] = user_id
+        session['firstname'] = first_name
+
+        print(session)
+        return redirect('/')
+    return render_template('login.html', logged_in=is_logged_in())
+
+
+@app.route('/signup', methods=['POST', 'GET'])
+def render_signup():
+    if is_logged_in():
+        return redirect('/menu/1')
+    if request.method == 'POST':
+        print(request.form)
+        fname = request.form.get('fname').title()
+        lname = request.form.get('lname').title().strip()
+        email = request.form.get('email').lower().strip()
+        password = request.form.get('password')
+        password2 = request.form.get('password2')
+
+        if password != password2:
+            return redirect("\signup?error=Passwords+do+not+match")
+
+        if len(password) < 8:
+            return redirect("\signup?error=Password+must+be+at+least+8+characters")
+
+        hashed_password = bcrypt.generate_password_hash(password)
+        con = create_connection(DATABASE)
+        query = "INSERT INTO user (fname, lname, email, password) VALUES (?, ?, ?, ?)"
+        cur = con.cursor()
+
+        try:
+            cur.execute(query, (fname, lname, email, hashed_password))
+        except sqlite3.IntegrityError:
+            con.close()
+            return redirect('\signup?error=Email+is+already+used')
+
+        con.commit()
+        con.close()
+
+        return redirect("\login")
+
+    return render_template('signup.html', logged_in=is_logged_in())
+
+
+@app.route('/logout')
+def logout():
+    print(list(session.keys()))
+    [session.pop(key) for key in list(session.keys())]
+    print(list(session.keys()))
+    return redirect('/?message=See+you+next+time!')
+
